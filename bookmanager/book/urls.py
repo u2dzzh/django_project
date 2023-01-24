4d17bc3bc888842633f688c7c3337f7aeeda9b6b
stu <484646414@qq.com>
2023-01-24 23:17:36 +0800

    子路由

---
+++ bookmanager/book/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from book.views import index
+
+urlpatterns = [
+    # path(路由, 视图函数名)
+    path('index/', index),
+]
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from book.views import index
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path(路由, 视图函数名)
-    path('index/', index),
+    path('', include('book.urls')),
 ]
