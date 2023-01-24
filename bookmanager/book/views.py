fb1999690497e97e3e921ffd8bcd6fa7a0cd9497
stu <484646414@qq.com>
2023-01-24 22:59:12 +0800

    路由匹配 index

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -13,5 +13,5 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 
 # 期望用户输入http:127.0.0.1/8000/index 来访问视图函数
-def index():
+def index(request):
     return HttpResponse('ok')
