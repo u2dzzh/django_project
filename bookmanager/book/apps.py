ddc746973efc15767ebd03b22bd6d3de566b8359
stu <484646414@qq.com>
2023-01-25 15:52:50 +0800

    apps config

--- bookmanager/book/apps.py
+++ bookmanager/book/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class BookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'book'
+    # 修改book的标题
+    verbose_name = '书籍管理'
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book.apps.BookConfig', #方案二
+    'book.apps.BookConfig', #方案二 同时加载BookConfig这个类
     # hahahahha
 ]
 
