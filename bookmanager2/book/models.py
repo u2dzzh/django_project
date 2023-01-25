37712cc3b093fbdddec5b91d4272341e9f6dab7f
stu <484646414@qq.com>
2023-01-25 19:30:34 +0800

    PeopleInfo_prepartion

--- bookmanager2/book/models.py
+++ bookmanager2/book/models.py
@@ -27,3 +27,26 @@ class BookInfo(models.Model):
         db_table = 'bookinfo'
         verbose_name = '书籍管理' # admin站点使用
 
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10, unique=True)
+    # 定义一个有序字典  枚举类型
+    GENDER_CHOICE = (
+        (1,'male'),
+        (2,'female')
+    )
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
+    # 系统会自动为外键添加_id
+    """
+    外键的级联操作  主表对从表  1对多
+    主表的一条数据 如果删除了  从表有关联的数据 关联的数据处理
+    SET_NULL  置空
+    抛出异常    不让删除
+    CASCADE    级联删除, 一起删除
+    """
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '人物信息'
