a302ad7d8e6546d765e2175a2e6f9d9c39773bdf
stu <484646414@qq.com>
2023-01-25 17:50:17 +0800

    mysql_preparation

--- bookmanager2/bookmanager2/settings.py
+++ bookmanager2/bookmanager2/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +75,12 @@ WSGI_APPLICATION = 'bookmanager2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',# ORM 改成mysql即可
+        'HOST': 'Localhost',
+        'PORT': 3306,
+        'USER': 'stu',
+        'PASSWORD': 'stu',
+        'NAME': 'book',
     }
 }
 
